70aeae222b6c0412fb154fc157072c8432f451cf
Максим Маркелов <M.Markelov@iepi.ru>
2022-08-01 20:47:16 +0300

    Initial commit

---
+++ hw-3.1/hw-3.1.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Calculator {
+public:
+    double num1;
+    double num2;
+
+    double add() {
+        return num1 + num2;
+    }
+
+    double multiply() {
+        return num1 * num2;
+    }
+
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+
+    double divide_1_2() {
+        return num1 / num2;
+    }
+
+    double divide_2_1() {
+        return num2 / num1;
+    }
+
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Calculator calculator;
+    double num;
+    boolean error = false;
+
+    while (true) {
+        do {
+            if (error) {
+                std::cout << "Неверный ввод!\n";
+            }
+            std::cout << "Введите num1: ";
+            std::cin >> num;
+            error = !calculator.set_num1(num);
+        } while (error);
+        error = false;
+
+        do {
+            if (error) {
+                std::cout << "Неверный ввод!\n";
+            }
+            std::cout << "Введите num2: ";
+            std::cin >> num;
+            error = !calculator.set_num2(num);
+        } while (error);
+        error = false;
+
+        std::cout << "num1 + num2 = " << calculator.add() << "\n";
+        std::cout << "num1 - num2 = " << calculator.subtract_1_2() << "\n";
+        std::cout << "num2 - num1 = " << calculator.subtract_2_1() << "\n";
+        std::cout << "num1 * num2 = " << calculator.multiply() << "\n";
+        std::cout << "num1 / num2 = " << calculator.divide_1_2() << "\n";
+        std::cout << "num2 / num1 = " << calculator.divide_2_1() << "\n";
+    }
+}
---
+++ hw-3.2/hw-3.2.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Counter {
+private:
+    int counts;
+public:
+    Counter() {
+        counts = 1;
+    };
+
+    Counter(int initialValue) {
+        counts = initialValue;
+    }
+
+    void decrease() {
+        counts--;
+    }
+
+    void increase() {
+        counts++;
+    }
+
+    int getCounts() {
+        return counts;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string answer;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> answer;
+
+    Counter counter;
+
+    if (answer == "да") {
+        int initValue;
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> initValue;
+        counter = Counter(initValue);
+    }
+    else {
+        if (answer == "нет") {
+            counter = Counter();
+        }
+        else {
+            std::cout << "Ответ непонятен. Начальное значение счётчика установлено по умолчанию.\n";
+            counter = Counter();
+        }
+    }
+
+    char ans[2] = "x";
+    bool stop = false;
+    do {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> ans;
+        switch ((int)(ans[0])) {
+        case static_cast<int>('+'):
+            counter.increase();
+            break;
+        case static_cast<int>('-'):
+            counter.decrease();
+            break;
+        case static_cast<int>('='):
+            std::cout << counter.getCounts() << "\n";
+            break;
+        case static_cast<int>('x'):
+        case static_cast<int>('х'):
+            stop = true;
+            break;
+        default:
+            std::cout << "Команда не распознана\n";
+        }
+    } while (!stop);
+    std::cout << "До свидания!" << "\n";
+}
